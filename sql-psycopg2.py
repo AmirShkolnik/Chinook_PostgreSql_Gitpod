12831e1a60a9ca451ab330f7f736fb688eb55ee5
Amir Shkolnik <amirshkolnik1@gmail.com>
2024-03-29 15:57:33 +0000

    create sql-expression file

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -7,23 +7,23 @@ connection = psycopg2.connect(database="chinook")
 # build a cursor object of the database
 cursor = connection.cursor()
 
-# Query 1 - select all records from the "Artist" table
-cursor.execute('SELECT * FROM "artist"')
+# Query 1 - select all records from the "artist" table
+# cursor.execute('SELECT * FROM "artist"')
 
 # Query 2 - select only the "Name" column from the "Artist" table
-# cursor.execute('SELECT "Name" FROM "Artist"')
+# cursor.execute('SELECT "name" FROM "artist"')
 
 # Query 3 - select only "Queen" from the "Artist" table
-# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+# cursor.execute('SELECT * FROM "artist" WHERE "name" = %s', ["Queen"])
 
 # Query 4 - select only by "ArtistId" #51 from the "Artist" table
-# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+cursor.execute('SELECT * FROM "artist" WHERE "artistId" = %s', [51])
 
 # Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
-# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+# cursor.execute('SELECT * FROM "album" WHERE "artistId" = %s', [51])
 
 # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
-cursor.execute('SELECT * FROM "Track" WHERE "composer" = %s', ["Queen"])
+# cursor.execute('SELECT * FROM "track" WHERE "composer" = %s', ["Queen"])
 
 # fetch the results (multiple)
 results = cursor.fetchall()
